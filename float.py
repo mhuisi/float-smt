e344a3bdfa92a2ec5e60b3e0e6c6b943182bf18a
mhuisi <mhuisi@protonmail.com>
2020-06-09 22:26:40 +0200

    add two constructors

--- float.py
+++ float.py
@@ -17,5 +17,23 @@ def FloatSort(mantissa_size : int, exponent_size : int) -> DatatypeSortRef:
                  ("exponent", BitVecSort(exponent_size)))
     return Float.create()
 
+def sizes(sort : DatatypeSortRef):
+    c = sort.constructor(0)
+    return (c.domain(1).size(), c.domain(2).size())
+
 def Float(name : str, mantissa_size : int, exponent_size : int):
     return Const(name, FloatSort(mantissa_size, exponent_size))
+
+def FloatVal(sign : bool, mantissa : int, exponent : int, sort : DatatypeSortRef):
+    return sort.mk(BoolVal(sign), BitVecVal(mantissa), BitVecVal(exponent))
+
+def FloatValDec(dec_val : str, rounding_mode : converter.RoundingMode, sort : DatatypeSortRef):
+    mantissa_size, exponent_size = sizes(sort)
+    f = converter.convert(dec_val, rounding_mode, mantissa_size, exponent_size)
+    assert(len(f.s) == 1)
+    assert(len(f.m) == mantissa_size)
+    assert(len(f.e) == exponent_size)
+    # this is a suboptimal conversion 
+    # (converter converts ints to binary strings, here we convert strings back to int)
+    # but we can get around to fixing this at some later point in the decoder
+    return FloatVal(f.s == "1", int(f.m, 2), int(f.e, 2), sort)
